cc8f2c1e14c1523c38b6c3e46eec09333c4328ed
Omar Chadidi <omarchadidi@gmail.com>
2020-01-27 02:43:38 +0100

    webpack config to compile bulme scss files into css.

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,46 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 
 module.exports = {
-    mode: 'development',
-    entry: './src/js/index.js',
-    output: {
-        filename: 'main.[contentHash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-          {
-            test: /\.s[ac]ss$/i,
-            use: [
-              // Creates `style` nodes from JS strings
-              'style-loader',
-              // Translates CSS into CommonJS
-              'css-loader',
-              // Compiles Sass to CSS
-              'sass-loader',
-            ],
-          },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Home page',
-            template: './src/homepage.html'
-        }),
-        new CleanWebpackPlugin()
-    ],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
-      }
+	mode: 'development',
+	entry: './src/js/index.js',
+	output: {
+		filename: 'main.[contentHash].js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [{
+			test: /\.scss$/,
+			use: [
+				MiniCssExtractPlugin.loader,
+				{
+					loader: 'css-loader'
+				},
+				{
+					loader: 'sass-loader',
+					options: {
+						sourceMap: true
+					}
+				}
+			]
+		}]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Home page',
+			template: './src/homepage.html'
+		}),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/mystyles.css'
+        })
+	],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 9000
+	}
 }
